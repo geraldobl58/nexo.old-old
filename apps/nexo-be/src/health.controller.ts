2beeb765c2b5adf9ab28eaa8bcc8b6a4e1b84242
Geraldo Luiz <geraldobl58@gmail.com>
2026-01-30 14:58:44 -0300

    fix: update deployment success messages to use emoji for consistency

--- apps/nexo-be/src/health.controller.ts
+++ apps/nexo-be/src/health.controller.ts
@@ -6,7 +6,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 export class HealthController {
   @Get('health')
   @ApiOperation({
-    summary: 'Deploy Automático Funcionando! 1🚀',
+    summary: '🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀',
   })
   @ApiResponse({
     status: 200,
--- apps/nexo-fe/src/app/page.tsx
+++ apps/nexo-fe/src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <div>Deploy Automático Funcionando! 1🚀</div>
+      <div>🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀</div>
     </>
   );
 };
