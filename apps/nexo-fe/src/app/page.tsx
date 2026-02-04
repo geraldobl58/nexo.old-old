9ae0aa8237e23555d52a4e4029fd07f0f1a7f2d3
Geraldo Luiz <geraldobl58@gmail.com>
2026-02-04 09:35:28 -0300

    fix: update deployment message to Version 20 in HomePage component

--- apps/nexo-fe/src/app/page.tsx
+++ apps/nexo-fe/src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <div>Deploy funcionando show de bola - Version 19 🚀</div>
+      <div>Deploy funcionando show de bola - Version 20 🚀</div>
     </>
   );
 };
