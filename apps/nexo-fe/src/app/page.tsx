eed71f986c037301ffd206354cc497a9c711a85f
Geraldo Luiz <geraldobl58@gmail.com>
2026-02-05 19:04:54 -0300

    fix: update version message in HomePage component to reflect PR version 2

--- apps/nexo-fe/src/app/page.tsx
+++ apps/nexo-fe/src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <div>Deploy funcionando show de bola - Version PR 1🚀🚀</div>
+      <div>Deploy funcionando show de bola - Version PR 2🚀🚀</div>
     </>
   );
 };
